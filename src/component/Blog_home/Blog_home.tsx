5e417b15d15cb87c8a38de4000dd1ec14895acdf
Irusha <mail.irusha@gmail.com>
2023-02-14 11:48:13 +0530

    Updated repository

--- src/component/Blog_home/Blog_home.tsx
+++ src/component/Blog_home/Blog_home.tsx
@@ -2,12 +2,15 @@ import './blog_home.scss'
 // img
 import Blog_1 from '../../assets/img/work-5.svg'
 // Icons
-import {CgFileDocument} from 'react-icons/cg'
-import {FaFacebookSquare} from 'react-icons/fa'
+import { CgFileDocument } from 'react-icons/cg'
+import { FaFacebookSquare } from 'react-icons/fa'
 function Blog_home() {
     return (
         <section className="blog__lite section">
-            <h1 className="sect__title">Some Blogs</h1>
+            <div className="sect__header">
+                <h1 className="sect__title">Some Blogs</h1>
+                <a href="/">View More blogs</a>
+            </div>
             <div className="grid blog__lite-grid">
 
                 <div className="a-post">
@@ -21,8 +24,8 @@ function Blog_home() {
                     <p className="discription">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi tempore harum voluptates quis aspernatur delectus repellat eum, quam voluptatem eius!</p>
 
                     <div className="btns">
-                        <button className="btn blog"><CgFileDocument/> <span>Blog</span></button>
-                        <button className="btn fb"><FaFacebookSquare/><span>FB</span></button>
+                        <button className="btn blog"><CgFileDocument /> <span>Blog</span></button>
+                        <button className="btn fb"><FaFacebookSquare /><span>FB</span></button>
                     </div>
                 </div>
                 <div className="a-post">
@@ -36,8 +39,8 @@ function Blog_home() {
                     <p className="discription">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi tempore harum voluptates quis aspernatur delectus repellat eum, quam voluptatem eius!</p>
 
                     <div className="btns">
-                        <button className="btn blog"><CgFileDocument/> <span>Blog</span></button>
-                        <button className="btn fb"><FaFacebookSquare/><span>FB</span></button>
+                        <button className="btn blog"><CgFileDocument /> <span>Blog</span></button>
+                        <button className="btn fb"><FaFacebookSquare /><span>FB</span></button>
                     </div>
                 </div>
                 <div className="a-post">
@@ -51,8 +54,8 @@ function Blog_home() {
                     <p className="discription">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi tempore harum voluptates quis aspernatur delectus repellat eum, quam voluptatem eius!</p>
 
                     <div className="btns">
-                        <button className="btn blog"><CgFileDocument/> <span>Blog</span></button>
-                        <button className="btn fb"><FaFacebookSquare/><span>FB</span></button>
+                        <button className="btn blog"><CgFileDocument /> <span>Blog</span></button>
+                        <button className="btn fb"><FaFacebookSquare /><span>FB</span></button>
                     </div>
                 </div>
                 <div className="a-post">
@@ -66,8 +69,8 @@ function Blog_home() {
                     <p className="discription">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi tempore harum voluptates quis aspernatur delectus repellat eum, quam voluptatem eius!</p>
 
                     <div className="btns">
-                        <button className="btn blog"><CgFileDocument/> <span>Blog</span></button>
-                        <button className="btn fb"><FaFacebookSquare/><span>FB</span></button>
+                        <button className="btn blog"><CgFileDocument /> <span>Blog</span></button>
+                        <button className="btn fb"><FaFacebookSquare /><span>FB</span></button>
                     </div>
                 </div>
                 <div className="a-post">
@@ -81,8 +84,8 @@ function Blog_home() {
                     <p className="discription">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi tempore harum voluptates quis aspernatur delectus repellat eum, quam voluptatem eius!</p>
 
                     <div className="btns">
-                        <button className="btn blog"><CgFileDocument/> <span>Blog</span></button>
-                        <button className="btn fb"><FaFacebookSquare/><span>FB</span></button>
+                        <button className="btn blog"><CgFileDocument /> <span>Blog</span></button>
+                        <button className="btn fb"><FaFacebookSquare /><span>FB</span></button>
                     </div>
                 </div>
                 <div className="a-post">
@@ -96,8 +99,8 @@ function Blog_home() {
                     <p className="discription">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi tempore harum voluptates quis aspernatur delectus repellat eum, quam voluptatem eius!</p>
 
                     <div className="btns">
-                        <button className="btn blog"><CgFileDocument/> <span>Blog</span></button>
-                        <button className="btn fb"><FaFacebookSquare/><span>FB</span></button>
+                        <button className="btn blog"><CgFileDocument /> <span>Blog</span></button>
+                        <button className="btn fb"><FaFacebookSquare /><span>FB</span></button>
                     </div>
                 </div>
 
